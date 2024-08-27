ea55a27458114a454ee597ba6e3fb52f0230e18f
felipestawinski <felipe.stawinski@gmail.com>
2024-08-27 16:05:40 -0300

    Using reflection to handle smart contract requests

--- cmd/api/handlers/blockchain-interaction.go
+++ cmd/api/handlers/blockchain-interaction.go
@@ -5,8 +5,10 @@ import (
 	"crypto/ecdsa"
 	"fmt"
 	"log"
+	"encoding/json"
 	"math/big"
 	"net/http"
+	"reflect"
 	"strings"
 
 	"github.com/BloxBerg-UTFPR/API-Blockchain/cmd/api/contract"
@@ -14,89 +16,150 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/core/types"
 )
+
+var readMethods = map[string]bool{
+	"DOMAIN_SEPARATOR": true,
+	"Allowance":        true,
+	"BalanceOf":        true,
+	"Decimals":         true,
+	"Eip712Domain":     true,
+	"Name":             true,
+	"Nonces":           true,
+	"Owner":            true,
+	"Paused":           true,
+	"Symbol":           true,
+	"TotalSupply":      true,
+}
 
 // welcomeHandler handles welcome requests for logged-in users
 func BlockchainInteraction(w http.ResponseWriter, r *http.Request) {
-	// separar em métodos de leitura e escrita
 	//Make sure to remove the prefix '0x' of private key 
 	//Eg.: 0xb13b0008c5cb0379f1d3a427bbfc75838a50eea795cf549c17b25e2c350c2e83 -> b13b0008c5cb0379f1d3a427bbfc75838a50eea795cf549c17b25e2c350c2e83
-    accountAddress := "0x42A2069C3F18DCd7e3a61276A8401bE431958239"
 	contractAddress := "0x5BcC1E2133119cb819c97418B73C770727001FBd"
-	teste := "a5058f2733cf8c615730962e75ed4af277df4983a9243298cd1c77afa621681f"
-
+	
 	UserAuthorized(w, r)
 
 	action := r.PathValue("method")
-	fmt.Println("method: ", action)
-
-	ethclient := ConnectToEthereum()
-	conn, err := contract.NewContract(common.HexToAddress(contractAddress), ethclient)
-		if err != nil {
-			panic(err)
-		}
-	
 	if action == "" {
-		http.Error(w, "Action should be provided", http.StatusUnauthorized)
-		return
-	} else if action == "name" {
-		res, err := conn.Name(&bind.CallOpts{})
-		if err != nil {
-			panic (err)
-		}
-		fmt.Fprintf(w, res)
-		w.WriteHeader(http.StatusOK)
-		fmt.Println(res)
-	} else if action == "nonces" {
-		res, err := conn.Nonces(&bind.CallOpts{}, common.HexToAddress((accountAddress)))
-		if err != nil {
-			panic (err)
-		}
-		fmt.Println("Nonces", res)
-		w.WriteHeader(http.StatusOK)
-	} else if action == "owner" {
-		res, err := conn.Owner(&bind.CallOpts{})
-		if err != nil {
-			panic (err)
-		}
-		fmt.Println("Owner: ", res)
-		w.WriteHeader(http.StatusOK)
-		fmt.Println(res)
-	} else if action == "paused" {
-		res, err := conn.Paused(&bind.CallOpts{})
-		if err != nil {
-			panic (err)
-		}
-		fmt.Println("Paused: ", res)
-		w.WriteHeader(http.StatusOK)
-		fmt.Println(res)
-	} else if action == "pause" {
-		auth := getAccountAuth(ethclient, teste)
-		res, err := conn.Pause(auth)
-
-		if err != nil {
-			panic (err)
-		}
-		fmt.Println("Pause: ", res)
-		w.WriteHeader(http.StatusOK)
-		fmt.Println(res)
-	} else if action == "unpause" {
-		auth := getAccountAuth(ethclient, teste)
-		res, err := conn.Unpause(auth)
-
-		if err != nil {
-			panic (err)
-		}
-		fmt.Println("Pause: ", res)
-		w.WriteHeader(http.StatusOK)
-		fmt.Println(res)
-	}else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "Unknown method:" + action)
+		http.Error(w, "Action should be provided", http.StatusBadRequest)
 		return
 	}
 
+	ethclient := ConnectToEthereum()
+	conn, err := contract.NewContract(common.HexToAddress(contractAddress), ethclient)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to instantiate contract: %v", err), http.StatusInternalServerError)
+	}
+
+	var result interface{}
+	var txn *types.Transaction
+
+	// Parse query parameters
+	params := make([]interface{}, 0)
+	for _, values := range r.URL.Query() {
+		if len(values) > 0 {
+			params = append(params, values[0])
+		}
+	}
+
+	if readMethods[action] {
+		result, err = callReadMethod(conn, action, params...)
+	} else {
+		//accountAddress := "0x42A2069C3F18DCd7e3a61276A8401bE431958239"
+		privateKey := "a5058f2733cf8c615730962e75ed4af277df4983a9243298cd1c77afa621681f"
+		auth := getAccountAuth(ethclient, privateKey)
+		txn, err = callWriteMethod(conn, action, auth, params...)
+	}
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error calling method %s: %v", action, err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if txn != nil {
+		json.NewEncoder(w).Encode(map[string]string{"txHash": txn.Hash().Hex()})
+	} else {
+		json.NewEncoder(w).Encode(result)
+	}
+	
 }
+
+func callReadMethod(conn *contract.Contract, methodName string, params ...interface{}) (interface{}, error) {
+	method := reflect.ValueOf(conn).MethodByName(methodName)
+	if !method.IsValid() {
+		return nil, fmt.Errorf("method %s not found", methodName)
+	}
+
+	inputs := make([]reflect.Value, len(params)+1)
+	inputs[0] = reflect.ValueOf(&bind.CallOpts{})
+	for i, param := range params {
+		inputs[i+1] = reflect.ValueOf(param)
+	}
+
+	results := method.Call(inputs)
+	if len(results) == 0 {
+		return nil, fmt.Errorf("method %s returned no results", methodName)
+	}
+
+	if err, ok := results[len(results)-1].Interface().(error); ok && err != nil {
+		return nil, err
+	}
+
+	return results[0].Interface(), nil
+}
+
+func callWriteMethod(conn *contract.Contract, methodName string, auth *bind.TransactOpts, params ...interface{}) (*types.Transaction, error) {
+	method := reflect.ValueOf(conn).MethodByName(methodName)
+	if !method.IsValid() {
+		return nil, fmt.Errorf("method %s not found", methodName)
+	}
+
+	inputs := make([]reflect.Value, len(params)+1)
+	inputs[0] = reflect.ValueOf(auth)
+
+	methodType := method.Type()
+	for i := 0; i < len(params); i++ {
+		paramType := methodType.In(i + 1)
+		convertedParam, err := convertParam(params[i], paramType)
+		if err != nil {
+			return nil, fmt.Errorf("error converting parameter %d: %v", i, err)
+		}
+		inputs[i+1] = reflect.ValueOf(convertedParam)
+	}
+
+	results := method.Call(inputs)
+	if len(results) == 0 {
+		return nil, fmt.Errorf("method %s returned no results", methodName)
+	}
+
+	if err, ok := results[len(results)-1].Interface().(error); ok && err != nil {
+		return nil, err
+	}
+
+	return results[0].Interface().(*types.Transaction), nil
+}
+
+func convertParam(param interface{}, targetType reflect.Type) (interface{}, error) {
+	switch targetType {
+	case reflect.TypeOf(common.Address{}):
+		if str, ok := param.(string); ok {
+			return common.HexToAddress(str), nil
+		}
+	case reflect.TypeOf(&big.Int{}):
+		if str, ok := param.(string); ok {
+			n, ok := new(big.Int).SetString(str, 10)
+			if !ok {
+				return nil, fmt.Errorf("invalid big.Int string: %s", str)
+			}
+			return n, nil
+		}
+	}
+	return param, nil
+}
+
 
 func ConnectToEthereum() *ethclient.Client {
     client, err := ethclient.Dial("https://polygon-amoy.g.alchemy.com/v2/5N0az1E28WsPnYk7g37TnNSNs3QZwKMr")
