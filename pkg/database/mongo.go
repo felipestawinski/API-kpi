b128b9415926010a9541a6fb8c152617e10c4dfb
Gustavo Henrique <gustavohenrisantos@gmail.com>
2025-05-10 21:26:50 -0300

    update uri

--- pkg/database/mongo.go
+++ pkg/database/mongo.go
@@ -1,26 +1,28 @@
 package database
 
 import (
-"context"
-"time"
-"go.mongodb.org/mongo-driver/mongo"
-"go.mongodb.org/mongo-driver/mongo/options"
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const MongoURI = "mongodb://localhost:27017"
-const DbName = "userdb"
-const CollectionName = "users"
+const MongoURI = "mongodb+srv://edfisic:DwyZQ0wzP3veQEWf@db-1.5di98i7.mongodb.net/?retryWrites=true&w=majority&appName=DB-1"
+const DbName = "Ed_Fisic"
+const CollectionName = "Ed_Fisic"
+
 var client *mongo.Client
 
 // NewMongoDB initializes a new MongoDB client.
 func NewMongoDB(uri string) *mongo.Client {
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-    client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
-    if err != nil {
-        panic(err)
-    }
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
 
-    return client
+	return client
 }
